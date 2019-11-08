593755bf01fbac4ab79999e66e2715e2c0a5e404
StasAlex <stas.alex28@gmail.com>
2019-11-08 16:01:30 +0200

    first commit test-project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const menuToggle = document.querySelector('#menu-toggle');
 const mobileNavContainer = document.querySelector('#mobile-nav');
+const body = document.querySelector('body');
 
-menuToggle.onclick = function (params) {
+// body.onclick = function () {
+//   if (mobileNavContainer.classList.contains('mobile-nav--active')) {
+//     console.log(mobileNavContainer.classList);
+//     menuToggle.classList.toggle('menu-icon-active');
+//     mobileNavContainer.classList.toggle('mobile-nav--active');
+//   }
+// }
+
+menuToggle.onclick = function () {
   menuToggle.classList.toggle('menu-icon-active');
   mobileNavContainer.classList.toggle('mobile-nav--active');
 }
